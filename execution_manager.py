6936f9502e4e9c8366798d06db32b21c1641329d
Lucas Martello Nogueira <lucas.martello0509@gmail.com>
2024-06-25 16:53:56 -0300

    add script para criar dados em csv

---
+++ execution_manager.py
@@ -0,0 +1,78 @@
+import pandas as pd
+import subprocess
+import os
+import time
+from tqdm import tqdm
+
+
+def create_file(filename: str) -> None:
+    if os.path.exists(filename) is False:
+        df = pd.DataFrame(columns=["program", "threads", "time", "n", "speed-up", "eficiencia"])
+        df.to_csv(filename, index=False)
+
+
+def run_file(input_filename: str, output_filename: str, n: int, num_threads: int) -> None:
+    with open(output_filename, "a") as f:
+        subprocess.call([input_filename, str(n), str(num_threads)], stdout=f)
+
+
+def run_executions(input_filename: str, output_filename: str, threads_list: list[int], n_list: list[int]):
+
+    for num_thread in threads_list:
+        for n in n_list:
+            run_file(input_filename, output_filename, n, num_thread)
+
+
+def run_executions_v2(input_filename: str, output_filename: str, combinations):
+    for n, num_threads in tqdm(combinations, total=len(combinations)):
+        run_file(input_filename, output_filename, n, num_threads)
+
+
+def read_data(filename):
+    df = pd.read_csv(filename)
+    print(df)
+
+
+def calculate_metrics(filename: str):
+    df = pd.read_csv(filename)
+
+    nums = [10**6, 10**7, 10**8]
+    threads = [2, 4, 8]
+
+    for n in nums:
+        df2 = df[df["n"] == n]
+        # print(df2)
+        t1 = df2.loc[df2["threads"] == 1, "time"].values[0]
+        for t in threads:
+            # print(f"{t1=}")
+            tn = df2.loc[df2["threads"] == t, "time"].values[0]
+            # print(f"{tn=}")
+            speed_up = round(t1 / tn, 3)
+            efficiency = round(speed_up / t, 2)
+            
+            df.loc[(df["n"] == n) & (df["threads"] == t), "speed-up"] = speed_up
+            df.loc[(df["n"] == n) & (df["threads"] == t), "eficiencia"] = efficiency
+
+    print(df)
+    df.to_csv(filename, index=False)
+
+
+
+def main():
+    output_file = "data.csv"
+    input_naive = "naive.exe"
+    create_file(output_file)
+
+    nums = [10**6, 10**7, 10**8]
+    threads = [1, 2, 4, 8]
+    combinations = [(n, thread) for n in nums for thread in threads]
+
+    # run_executions(input_naive, output_file, threads, nums)
+    # run_executions_v2(input_naive, output_file, combinations)
+
+    # read_data(filename=output_file)
+    calculate_metrics(output_file)
+
+
+if __name__ == "__main__":
+    main()
--- openmp_primos.c
+++ openmp_primos.c
@@ -19,7 +19,7 @@ int main(int argc, char* argv[]) {
     int cont = 0;
 
     if (argc < 2) {
-        printf("Valor inválido! Entre com um valor do maior inteiro\n");
+        // printf("Valor inválido! Entre com um valor do maior inteiro\n");
        	return 0;
     }else {
         n = strtol(argv[1], (char **) NULL, 10);
@@ -31,16 +31,17 @@ int main(int argc, char* argv[]) {
     #pragma omp parallel for shared(cont) num_threads(n_threads)
     for (i = 2; i <= n; i += 2) {	
         if(primo(i) == 1){
-            printf("achou primo: %d / thread: %d\n", i, omp_get_thread_num());
+            // printf("achou primo: %d / thread: %d\n", i, omp_get_thread_num());
             cont++;   
         }
     }
 
     t_final = omp_get_wtime();
+    double time = t_final - t_inicial;
 
-    printf("quantidade total de primos: %d\n", cont);
-    printf("tempo decorrido: %f\n", t_final - t_inicial);
-
+    // printf("quantidade total de primos: %d\n", cont);
+    // printf("tempo decorrido: %f\n", t_final - t_inicial);
+    printf("Naive,%ld,%.3f,%ld\n",n_threads,time,n);
 
     return 0;
 }
